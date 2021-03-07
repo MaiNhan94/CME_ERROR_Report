bdc42ce8b26cc78377e8e733c1611f293ea0e7ab
MaiNhanIMIC <Nhan@250594>
2021-03-07 11:42:28 +0700

    change to client's server

--- CME_ERROR_Report.py
+++ CME_ERROR_Report.py
@@ -1,32 +1,37 @@
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 import json
 import datetime
+import os
 
 def customCallback(client, userdata, message):
         topic = message.topic
         payload = message.payload.decode("utf-8")
         payload_json = json.loads(payload)
         if(payload_json["eventType"] == "connected"):
-            print("connected")
+            print(str(datetime.datetime.utcnow()) + " - connected")
             File_Connect = open("Connected.log", "a")
             File_Connect.write(str(datetime.datetime.utcnow()))
             File_Connect.write(" - ")
             File_Connect.writelines(payload)
             File_Connect.close()
         if(payload_json["eventType"] == "disconnected"):
-            print("disconnected")
+            print(str(datetime.datetime.utcnow()) + " - disconnected")
             File_DisConnect = open("Disconnected.log", "a")
             File_DisConnect.write(str(datetime.datetime.utcnow()))
             File_DisConnect.write(" - ")
             File_DisConnect.writelines(payload)
             File_DisConnect.close()
 
-ENDPOINT = "a3sn7spt8abe46-ats.iot.us-east-2.amazonaws.com"
+ENDPOINT = "a1q3rplqhm9yv-ats.iot.ap-northeast-1.amazonaws.com"
             
 CLIENT_ID = "CME_ERROR_Report_Python_ID"
-PATH_TO_CERT = "C:\\Users\\NhanIMIC\\Desktop\\IoT_iMIC\\31fc29c16a-certificate.pem.crt"
-PATH_TO_KEY = "C:\\Users\\NhanIMIC\\Desktop\\IoT_iMIC\\31fc29c16a-private.pem.key"
-PATH_TO_CA = "C:\\Users\\NhanIMIC\\Desktop\\IoT_iMIC\\AmazonRootCA1.pem"
+#PATH_TO_CERT = "C:\\Users\\NhanIMIC\\Desktop\\IoT_iMIC\\31fc29c16a-certificate.pem.crt"
+#PATH_TO_KEY = "C:\\Users\\NhanIMIC\\Desktop\\IoT_iMIC\\31fc29c16a-private.pem.key"
+#PATH_TO_CA = "C:\\Users\\NhanIMIC\\Desktop\\IoT_iMIC\\AmazonRootCA1.pem"
+
+PATH_TO_CERT = str(os.getcwd()) + "\\certificate\\986a9699b8-certificate.pem.crt"
+PATH_TO_KEY = str(os.getcwd()) + "\\certificate\\986a9699b8-private.pem.key"
+PATH_TO_CA = str(os.getcwd()) + "\\certificate\\AmazonRootCA1.pem"
 
 DisConnect_Topic = "$aws/events/presence/disconnected/+"
 Connect_Topic = "$aws/events/presence/connected/+"
